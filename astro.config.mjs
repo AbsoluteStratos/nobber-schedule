c1924fdb63d5713e43106472381d6bb56a67087a
Absolute Stratos <absolutestratos@gmail.com>
2025-09-06 09:21:44 -0700

    Fixing config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://absolutestratos.github.io/nobber-schedule/",
+    site: "https://absolutestratos.github.io/",
+    base: "nobber-schedule/",
     integrations: [react(), icon()],
 });
