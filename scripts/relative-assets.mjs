2fff278aeed668be7c8361f7e3ce55e7f37c7230
Absolute Stratos <absolutestratos@gmail.com>
2026-09-04 18:11:08 -0700

    relative fix

--- scripts/relative-assets.mjs
+++ scripts/relative-assets.mjs
@@ -3,25 +3,52 @@ import { join } from "node:path";
 
 const dist = "dist";
 
-async function htmlFiles(dir) {
+async function walk(dir) {
   const entries = await readdir(dir, { withFileTypes: true });
   const files = [];
   for (const entry of entries) {
     const path = join(dir, entry.name);
     if (entry.isDirectory()) {
-      files.push(...(await htmlFiles(path)));
-    } else if (entry.name.endsWith(".html")) {
+      files.push(...(await walk(path)));
+    } else {
       files.push(path);
     }
   }
   return files;
 }
 
-const files = await htmlFiles(dist);
+function relativizeHtml(html) {
+  return html
+    .replaceAll('href="/2025/assets/', 'href="assets/')
+    .replaceAll("href='/2025/assets/", "href='assets/")
+    .replaceAll('src="/2025/assets/', 'src="assets/')
+    .replaceAll("src='/2025/assets/", "src='assets/")
+    .replaceAll("url('/2025/assets/", "url('./")
+    .replaceAll('url("/2025/assets/', 'url("./')
+    .replaceAll("url(/2025/assets/", "url(./")
+    .replaceAll("url('assets/", "url('./")
+    .replaceAll('url("assets/', 'url("./');
+}
+
+function relativizeCss(css) {
+  return css
+    .replaceAll("url('/2025/assets/", "url('./")
+    .replaceAll('url("/2025/assets/', 'url("./')
+    .replaceAll("url(/2025/assets/", "url(./")
+    .replaceAll("url('assets/", "url('./")
+    .replaceAll('url("assets/', 'url("./')
+    .replaceAll("url(assets/", "url(./");
+}
+
+const files = await walk(dist);
 for (const file of files) {
-  const html = await readFile(file, "utf8");
-  const next = html.replaceAll("/2025/assets/", "assets/");
-  if (next !== html) {
-    await writeFile(file, next);
+  if (file.endsWith(".html")) {
+    const html = await readFile(file, "utf8");
+    const next = relativizeHtml(html);
+    if (next !== html) await writeFile(file, next);
+  } else if (file.endsWith(".css")) {
+    const css = await readFile(file, "utf8");
+    const next = relativizeCss(css);
+    if (next !== css) await writeFile(file, next);
   }
 }
